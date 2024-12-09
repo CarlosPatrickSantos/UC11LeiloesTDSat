7b9feebc86e1d1e8e225089fd0029a7ebebaf243
Carlos Santos <tickss2002@gmail.com>
2024-12-09 11:36:23 -0300

    Adicionado botão de salvar cadastrando produto e Listagem de produtos fucionando.

--- LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/src/ProdutosDAO.java
@@ -12,18 +12,36 @@ import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 
 public class ProdutosDAO {
     
-    Connection conn;
-    PreparedStatement prep;
-    ResultSet resultset;
-    ArrayList<ProdutosDTO> listagem = new ArrayList<>();
+   private Connection conn;
+   private PreparedStatement prep;
+   private ResultSet resultset;
+   private ArrayList<ProdutosDTO> listagem = new ArrayList<>();
+   
+   public ProdutosDAO() {
+       
+       conectaDAO dao = new conectaDAO(); 
+        this.conn = dao.getConnection(); 
+        listagem = new ArrayList<>();
+   }
+           
     
     public void cadastrarProduto (ProdutosDTO produto){
+        String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
         
+        try {
+            PreparedStatement stmt = conn.prepareStatement(sql); 
+            stmt.setString(1, produto.getNome()); 
+            stmt.setInt(2, produto.getValor()); 
+            stmt.setString(3, produto.getStatus()); 
+            stmt.executeUpdate(); stmt.close(); 
+        } catch (SQLException e) { 
+            e.printStackTrace(); }
         
         //conn = new conectaDAO().connectDB();
         
@@ -31,9 +49,26 @@ public class ProdutosDAO {
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
-        
-        return listagem;
-    }
+       String sql = "SELECT * FROM produtos";
+     
+     try {
+         PreparedStatement stmt = conn.prepareStatement(sql);
+         ResultSet rs = stmt.executeQuery();
+     while (rs.next()) { 
+         ProdutosDTO produto = new ProdutosDTO();
+         produto.setId(rs.getInt("id")); 
+         produto.setNome(rs.getString("nome")); 
+         produto.setValor(rs.getInt("valor")); 
+         produto.setStatus(rs.getString("status")); 
+         listagem.add(produto);
+      }
+      
+          rs.close(); 
+          stmt.close(); 
+      } catch (SQLException e) { 
+          e.printStackTrace(); 
+      } return listagem;
+    } 
     
     
     
